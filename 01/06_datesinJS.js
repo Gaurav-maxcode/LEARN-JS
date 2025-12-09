9f23075d69d8c949ed89f8a3899450e2ef9bc6f8
Gaurav-maxcode <gauaryal@gmail.com>
2025-12-09 03:42:27 +0000

    Arrays

--- 01/06_datesinJS.js
+++ 01/06_datesinJS.js
@@ -1,10 +1,13 @@
+
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleTimeString());
+// console.log(mydate.toString());
+// console.log(mydate.toTimeString());
+
 let mydate = new Date();
 
-console.log(mydate.toDateString());
-console.log(mydate.toISOString());
-console.log(mydate.toJSON());
-console.log(mydate.toLocaleDateString());
-console.log(mydate.toLocaleString());
-console.log(mydate.toLocaleTimeString());
-console.log(mydate.toString());
-console.log(mydate.toTimeString());
+console.log(Math.round(Date.now()/1000/60/60/24/30/12)+1970);
---
+++ 02/01_Arrays.js
@@ -0,0 +1,3 @@
+const myArr = [1,2,3,4,5,6]
+let arrContainer = myArr.join
+console.log(arrContainer.replace(","," "));
